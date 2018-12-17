18b34ae61328741052120d104cdb6581bc4b180b
George Kobiashvili <giokob@gmail.com>
2018-12-17 15:26:32 +0400

    SO_REUSEPORT

--- server/server.go
+++ server/server.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"os"
+	"syscall"
 	"time"
+
+	"golang.org/x/sys/unix"
 
 	"github.com/mpps/utils"
 )
 
 var byteCount = 122
 
-func dataReceiver(conn *net.UDPConn, dataChannel chan<- []byte) {
+func dataReceiver(dataChannel chan<- []byte) {
+	lc := net.ListenConfig{
+		Control: listenCtrl,
+	}
+	conn, err := lc.ListenPacket(context.Background(), "udp4", "127.0.0.1:22222")
+	checkError(err)
+	defer conn.Close()
+
 	data := make([]byte, byteCount)
 	for {
-		_, _, err := conn.ReadFromUDP(data)
+		// _, _, err := conn.ReadFromUDP(data)
+		_, _, err := conn.ReadFrom(data)
 		checkError(err)
 		dataChannel <- data
 	}
@@ -46,14 +58,34 @@ func dataAnalyzer(dataChannel chan []byte) {
 	}
 }
 
+func listenCtrl(network string, address string, c syscall.RawConn) error {
+	var operr error
+	var fn = func(s uintptr) {
+		operr = unix.SetsockoptInt(int(s), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+	}
+
+	err := c.Control(fn)
+	if err != nil {
+		return err
+	}
+	if operr != nil {
+		return operr
+	}
+	return nil
+}
+
 func main() {
-	addr := net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 22222, Zone: ""}
-	conn, err := net.ListenUDP("udp", &addr)
-	checkError(err)
-	defer conn.Close()
+
+	// addr := net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 22222, Zone: ""}
+	// conn, err := net.ListenUDP("udp", &addr)
+	// checkError(err)
+	// defer conn.Close()
 
 	dataChannel := make(chan []byte, 10)
-	go dataReceiver(conn, dataChannel)
+	go dataReceiver(dataChannel)
+	go dataReceiver(dataChannel)
+	go dataReceiver(dataChannel)
+	go dataReceiver(dataChannel)
 	dataAnalyzer(dataChannel)
 }
 
